b222c23bf7439c1a24721fa5b5751e1b5f9e5a74
tbennett6421 <tbennett6421@gmail.com>
2019-04-02 15:50:44 -0500

    smtp enumeration

---
+++ Enumeration/SMTP/smtp-nse.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+    echo "[*] SMTP NSE"
+    echo "[*] Usage : $0 <target>"
+    exit 0
+fi
+
+nmap $1 -p 25,465,587 -sC -sV --script "smtp-commands, smtp-enum-users, smtp-ntlm-info, smtp-vuln-cve2010-4344, smtp-vuln-cve2011-1720, smtp-vuln-cve2011-1764" -oA $i.smtp
