9e6b4d24c9638b3efb33e8e2b7eb215ad00d00df
tbennett6421 <tbennett6421@gmail.com>
2019-04-02 17:15:07 -0500

    hotfix: pass user list in smtp enum

--- Enumeration/SMTP/smtp-auth.sh
+++ Enumeration/SMTP/smtp-auth.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ -z "$1" ]; then
+if [ $# -ne 2 ]; then
     echo "[*] SMTP Auth"
-    echo "[*] Usage : $0 <target>"
+    echo "[*] Usage : $0 <target> <userlist>"
     exit 0
 fi
 
-smtp-user-enum -M VRFY -U /usr/share/seclists/Usernames/top_shortlist.txt -t $1 -p 25
+smtp-user-enum -M VRFY -U $2 -t $1 -p 25
