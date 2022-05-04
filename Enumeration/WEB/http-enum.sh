8b7d96737c155576d705868907adf1adebcc8522
Tyler Bennett <tbennett6421@gmail.com>
2022-05-03 21:02:03 -0500

    fix typo in enum script

--- Enumeration/WEB/http-enum.sh
+++ Enumeration/WEB/http-enum.sh
@@ -9,4 +9,4 @@ curl -i $1:$2 2>&1 | tee $1_$2.headers.txt
 w3m -dump $1:$2/robots.txt | tee $1_$2_robots.txt
 VHostScan -t $1 -p $2 -oN $1_$2_vhosts.txt
 nikto -h $1 -p $2 -output $1_$2_nikto.txt
-dirb http://$1:$2/ -o $1_dirb.txt
+dirb http://$1:$2/ -o $1_$2_dirb.txt
