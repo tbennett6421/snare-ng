92bf07c1ebf1dc0b6f882a2223ccba95dcfabfe7
Tyler Bennett <tbennett6421@gmail.com>
2022-05-15 17:10:19 -0500

    list out spns

---
+++ Enumeration/Active Directory/list-spn.ps1
@@ -0,0 +1,20 @@
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+$PDC = ($domainObj.PdcRoleOwner).Name
+$SearchString = "LDAP://"
+$SearchString += $PDC + "/"
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+$SearchString += $DistinguishedName
+$Searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString)
+$objDomain = New-Object System.DirectoryServices.DirectoryEntry
+$Searcher.SearchRoot = $objDomain
+$Searcher.filter="serviceprincipalname=*"
+$Result = $Searcher.FindAll()
+Foreach($obj in $Result)
+{
+	Foreach($prop in $obj.Properties)
+	{
+		$prop 
+	}
+    write-host "==================="
+
+}
