b7e21402a5896eb700d4131fbf97cdc1622e8992
Tyler Bennett <tbennett6421@gmail.com>
2022-05-15 12:04:17 -0500

    fix nbtscan

--- Enumeration/SMB/nbtscan.sh
+++ Enumeration/SMB/nbtscan.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 if [ -z "$1" ]; then
     echo "[*] nbtscan subnet scan"
-    echo "[*] Usage : $0 <range/cidr>"
+    echo "[*] Usage: $0 <range/cidr>"
+    echo "[*] Example: $0 10.11.1.0/24"
     exit 0
 fi
 
-nbtscan $1 > >(tee -a $1.nbtscan.stdout.log) 2> >(tee -a $1.nbtscan.stderr.log >&2)
+substr="${1%%/*}"
+nbtscan $1 > >(tee -a $substr.nbtscan.stdout.log) 2> >(tee -a $substr.nbtscan.stderr.log >&2)
