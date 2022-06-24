ec82a8af1a96d7f0fe4e55370768e4b7bd19fcdb
tbennett6421 <tbennett6421@gmail.com>
2022-06-24 00:43:42 -0500

    fix typo

--- Tools/add-admin.bat
+++ Tools/add-admin.bat
@@ -2,9 +2,9 @@
 :: This batch file adds user to Administrator and enables RDP
 Title Add user
 echo Adding user...
-net user offsec password /adds
-net localgroup Administrators offsec /adds
-net localgroup "Remote Desktop Users" offsec /adds
+net user offsec password /add
+net localgroup Administrators offsec /add
+net localgroup "Remote Desktop Users" offsec /add
 ECHO Enabling RDP...
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
 ECHO ====================
