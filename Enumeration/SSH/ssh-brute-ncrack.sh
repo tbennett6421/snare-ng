5d6a08e1d7a9095655f0d5754070bce9a4b71508
tbennett6421 <tbennett6421@gmail.com>
2022-06-26 13:25:34 -0500

    consolidate information into offsecdb

--- Enumeration/SSH/ssh-brute-ncrack.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-if [ $# -ne 3 ]; then
-    echo "[*] SSH ncrack bruteforce"
-    echo "[*] Usage : $0 <target> <user_file> <pass_file>"
-    exit 0
-fi
-ncrack -vv -p 22 -U $2 -P $3 $1 -oA $1.ncrack
--- Enumeration/SSH/ssh-enum.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-if [ -z "$1" ]; then
-    echo "[*] SSH Enum"
-    echo "[*] Usage : $0 <target>"
-    exit 0
-fi
-
-nmap $1 -p 22 -sV -Pn --script=ssh-hostkey -oA $1.ssh-hostkey
